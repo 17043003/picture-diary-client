f41b3152620db055765bb31b63771e46937d0d4b
17043003 <ishizuka533725@gmail.com>
2022-02-27 16:05:59 +0900

    add new post page.

---
+++ pages/post/new.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+
+const NewPostPage: NextPage = () => {
+    return(
+        <div>
+            <main>
+              aaa
+            </main>
+        </div>
+    )
+}
+
+export default NewPostPage
