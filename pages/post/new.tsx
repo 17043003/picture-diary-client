5d528fd539f0be03c335a706c52acdcdefa6fb4c
17043003 <ishizuka533725@gmail.com>
2022-03-05 13:53:18 +0900

    add form to upload image to S3.

--- pages/post/new.tsx
+++ pages/post/new.tsx
@@ -1,41 +1,67 @@
-import type { NextPage } from "next";
-import useSWR from 'swr'
-import { useState } from "react"
-import { postFetcher } from "../../util/fetcher"
+import type { NextPage } from 'next';
+import useSWR from 'swr';
+import { useState } from 'react';
+import { postFetcher, uploadImage } from '../../util/fetcher';
 
 const NewPostPage: NextPage = () => {
-    const [title, setTitle] = useState("")
-    const [body, setBody] = useState("")
-    const handleChangeTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setTitle(e.target.value)
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [images, setImages] = useState<FileList>();
+
+  const handleChangeTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
+  const handleChangeBody = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setBody(e.target.value);
+  };
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const requestBody: BodyInit = `title=${encodeURIComponent(title)}&body=${encodeURIComponent(
+      body,
+    )}`;
+
+    // upload image
+    let imageLocation: string | null = null;
+    if (!images) return;
+    if (images.length !== 0) {
+      imageLocation = uploadImage(images);
     }
-    const handleChangeBody = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setBody(e.target.value)
+
+    if (imageLocation !== null) {
+      postFetcher('/api/post', requestBody + `&imageUrl=${encodeURIComponent(imageLocation)}`);
+    } else {
+      postFetcher('/api/post', requestBody);
     }
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const requestBody: BodyInit = `title=${encodeURIComponent(title)}&body=${encodeURIComponent(body)}`
-        const res = postFetcher('/api/post', requestBody);
-        alert(res)
+  };
+
+  const handleFile = (event: React.FormEvent<HTMLInputElement>) => {
+    const { files } = event.target as HTMLInputElement;
+    if (files) {
+      setImages(files);
     }
+  };
+
   return (
     <div>
       <main>
         <h1>日記作成</h1>
         <form onSubmit={handleSubmit}>
-          <label htmlFor="title">
-            <input type="text" name="title" id="input_title" onChange={handleChangeTitle} />
+          <label htmlFor='title'>
+            <input type='text' name='title' id='input_title' onChange={handleChangeTitle} />
           </label>
-          <label htmlFor="body">
+          <label htmlFor='body'>
             <textarea
-              name="body"
-              id="input_body"
+              name='body'
+              id='input_body'
               onChange={handleChangeBody}
-              cols="90"
-              rows="10"
+              cols={90}
+              rows={10}
             ></textarea>
           </label>
-          <input type="submit" value="save" />
+          <label htmlFor='image'>
+            <input type='file' onChange={handleFile} />
+          </label>
+          <input type='submit' value='save' />
         </form>
       </main>
     </div>
--- util/fetcher.ts
+++ util/fetcher.ts
@@ -1,23 +1,53 @@
-const baseUrl: RequestInfo = process.env.BASEURL ?? "http://localhost:8080"
+import aws, { S3 } from 'aws-sdk';
+
+const baseUrl: RequestInfo = process.env.BASEURL ?? 'http://localhost:8080';
 const headers = {
-    'Accept': 'application/json',
-    'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
-  };
+  Accept: 'application/json',
+  'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+};
 
 export const getFetcher = async (url: RequestInfo) => {
-    const res = await fetch(baseUrl + url, {
-        method: "GET"
-    })
-    .then(r => r.json())
-    return res;
-}
+  const res = await fetch(baseUrl + url, {
+    method: 'GET',
+  }).then((r) => r.json());
+  return res;
+};
 
 export const postFetcher = async (url: RequestInfo, body: BodyInit) => {
-    const res = await fetch(baseUrl + url, {
-        method: "POST",
-        headers,
-        body,
-    })
-    
-    return res.json();
-}
+  const res = await fetch(baseUrl + url, {
+    method: 'POST',
+    headers,
+    body,
+  });
+
+  return res.json();
+};
+
+export const uploadImage = (files: FileList): string | null => {
+  aws.config.logger = console;
+
+  const region = process.env.NEXT_PUBLIC_REGION ?? '';
+  const bucket = new S3({
+    accessKeyId: process.env.NEXT_PUBLIC_ACCESS_KEY,
+    secretAccessKey: process.env.NEXT_PUBLIC_SECRET_KEY,
+    region,
+  });
+
+  const bucketName = process.env.NEXT_PUBLIC_BUCKET_NAME ?? '';
+  const fileKey = files[0].name;
+  const param: S3.Types.PutObjectRequest = {
+    Bucket: bucketName,
+    Key: fileKey,
+    Body: files[0],
+    ContentType: 'image/jpeg',
+  };
+
+  bucket.upload(param, (err: Error, data: S3.ManagedUpload.SendData) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('Successfully uploaded file.', data);
+    }
+  });
+  return fileKey;
+};
