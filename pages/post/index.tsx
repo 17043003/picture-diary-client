af1f30be560b96b6e09636ffc3a32890990acd87
17043003 <ishizuka533725@gmail.com>
2022-03-06 18:05:03 +0900

    get post lists from DB.

--- pages/post/index.tsx
+++ pages/post/index.tsx
@@ -1,31 +1,35 @@
 import type { NextPage } from 'next';
-import Image from 'next/image'
+import Image from 'next/image';
+import { getFetcher } from '../../util/fetcher';
+import { useState, useEffect } from 'react';
 
-const IndexPostPage: NextPage = ({ posts }: any) => {
-  const post = (
-    <div>
-      <div className='container'>
-        <div className='image'></div>
-        <Image src="/images/avatar.png" width={64} height={64} alt="Post thumbnail" />
-        <div className='title'>{posts.title}</div>
-        <div className='body_summary'>{posts.body}</div>
-      </div>
-    </div>
-  );
-
-  return <div>{post}</div>;
+type Post = {
+  id: number;
+  title: String;
+  body: String;
+  imageUrl: String;
 };
 
-export async function getServerSideProps() {
-  return {
-    props: {
-      posts: {
-        id: 1,
-        title: 'test title',
-        body: 'test body',
-      },
-    },
-  };
-}
+const IndexPostPage: NextPage = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+  useEffect(() => {
+    getFetcher('/api/post').then((data) => setPosts(data));
+  }, []);
+
+  const postElements = posts?.map((post) => {
+    return (
+      <div key={post.id}>
+        <div className='container'>
+          <div className='image'></div>
+          <Image src='/images/avatar.png' width={64} height={64} alt='Post thumbnail' />
+          <div className='title'>{post.title}</div>
+          <div className='body_summary'>{post.body}</div>
+        </div>
+      </div>
+    );
+  });
+
+  return <div>{postElements}</div>;
+};
 
 export default IndexPostPage;
