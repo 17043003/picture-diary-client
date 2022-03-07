5c11265542d5e5bffebaa94f3fec009bff3e1f11
17043003 <ishizuka533725@gmail.com>
2022-03-08 02:27:16 +0900

    show image from S3.

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -1,11 +1,11 @@
 import type { NextPage } from 'next';
-import { getFetcher } from '../../util/fetcher';
-import { Post } from '../../util/post'
+import { getFetcher, downloadImage } from '../../util/fetcher';
+import { Post } from '../../util/post';
 
-const DetailPostPage: NextPage = ({ post }: { post: Post }) => {
+const DetailPostPage: NextPage = ({ post, uri }: { post: Post; uri: string }) => {
   return (
     <div>
-      <p>{post.imageUrls}</p>
+      <img src={uri} alt={post.imageUrls[0]} width={120} height={120} />
       <h1>{post.title}</h1>
       <p>{post.body}</p>
     </div>
@@ -15,7 +15,15 @@ const DetailPostPage: NextPage = ({ post }: { post: Post }) => {
 export async function getServerSideProps(context: any) {
   const { id } = context.query;
   const post: Post = await getFetcher(`/api/post/${id}`);
-  return { props: { post } };
+
+  const image = (await downloadImage(post.imageUrls[0])) ?? null;
+  if (image == null) return { props: { post, uri: '' } };
+
+  const buffer = Buffer.from(image);
+  const base64 = buffer.toString('base64');
+  const mime = 'image/jpeg';
+  const uri = `data:${mime};base64,` + base64;
+  return { props: { post, uri } };
 }
 
 export default DetailPostPage;
