286096d2da4a80bbdac53abe18239ce769967277
17043003 <ishizuka533725@gmail.com>
2022-03-14 22:34:25 +0900

    login using api.

---
+++ pages/api/login.ts
@@ -0,0 +1,21 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type Data = {
+  token: string;
+};
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const headers = {
+    Accept: 'application/json',
+    'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+  };
+  const baseUrl: RequestInfo = process.env.BASEURL ?? 'http://localhost:8080';
+  const data = await fetch(`${baseUrl}/login`, {
+    method: 'POST',
+    headers,
+    body: `email=${req.body.email}`,
+  });
+  const { token } = await data.json();
+  res.status(200).json({ token });
+}
