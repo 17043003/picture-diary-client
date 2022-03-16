11bd6efff06d9ccbefef38de368f613efa926480
17043003 <ishizuka533725@gmail.com>
2022-03-16 20:35:27 +0900

    show user info in mypage.

--- pages/user/mypage.tsx
+++ pages/user/mypage.tsx
@@ -1,5 +1,7 @@
 import type { NextPage } from 'next';
+import { GetServerSideProps } from 'next'
 import { User } from '../../util/user';
+import axios from "axios";
 
 const MyPage: NextPage = ({ user }: { user: User }) => {
   return (
@@ -10,8 +12,19 @@ const MyPage: NextPage = ({ user }: { user: User }) => {
   );
 };
 
-export async function getServerSideProps(context: any) {
-  return { props: { user: { name: 'test', email: 'test@gmail.com' } } };
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const headers = {
+    Accept: 'application/json',
+    Authorization: `Bearer ${context.req.cookies.token}`,
+  };
+  const baseUrl: RequestInfo = process.env.BASEURL ?? 'http://localhost:8080';
+
+  const user = await axios.get(`${baseUrl}/api/user`, {
+    headers
+  }).then(v => v.data)
+  .catch(null)
+  
+  return { props: { user } };
 }
 
 export default MyPage;
