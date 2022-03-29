8e3a70acf5ddd263dc0502010d2e857100e30a24
17043003 <ishizuka533725@gmail.com>
2022-03-30 02:39:52 +0900

    add css to login page.

--- pages/login.tsx
+++ pages/login.tsx
@@ -32,11 +32,28 @@ const Login: NextPage = () => {
     setEmail(event.target.value);
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor='email'>メールアドレス：</label>
-      <input type='email' name='email' onChange={changeEmail} />
-      <input type='submit' />
-    </form>
+    <div>
+      <h1 className='font-bold text-4xl p-10 bg-gradient-to-r from-lime-200 to-lime-400'>
+        ログイン
+      </h1>
+      <form onSubmit={handleSubmit} className='mt-10 mx-20'>
+        <div className='flex rounded-md'>
+          <label htmlFor='email' className='font-bold underline'>
+            メールアドレス：
+          </label>
+          <input
+            type='email'
+            name='email'
+            onChange={changeEmail}
+            className='focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300 shadow-md px-2 py-1'
+          />
+        </div>
+        <input
+          type='submit'
+          className='bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-3 mt-4 rounded'
+        />
+      </form>
+    </div>
   );
 };
 
