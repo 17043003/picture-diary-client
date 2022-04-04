f7a7c870bfe020ec1457532eb7efbf3792331046
17043003 <ishizuka533725@gmail.com>
2022-04-05 01:37:06 +0900

    check cookie to fonfirm login state.
    when cookie's token is empty, login state become false.
    It would redirect to login page, if login state were false.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,22 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { parseCookies, setCookie, destroyCookie } from 'nookies';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [loggedIn, setLoggedIn] = useState(false)
   const router = useRouter();
+  useEffect(() => {
+    const cookies = parseCookies();
+    setLoggedIn(cookies['token'] != null)
+  })
+
   useEffect(() => {
     const cookies = parseCookies();
     if (!cookies['token']) router.push('/login');
     return () => {};
-  });
+  },[loggedIn]);
   return <Component {...pageProps} />;
 }
 
