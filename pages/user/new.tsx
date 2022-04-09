1dfd862b2684fc924a12635d7a8efc2fd63b2b1c
17043003 <ishizuka533725@gmail.com>
2022-04-10 08:32:06 +0900

    replace input form to react hook form in new user page.

--- pages/user/new.tsx
+++ pages/user/new.tsx
@@ -1,75 +1,68 @@
 import type { NextPage } from 'next';
-import { useState } from 'react';
 import { postFetcher } from '../../util/fetcher';
+import { Path, useForm, UseFormRegister, SubmitHandler } from 'react-hook-form';
 
 const NewUserPage: NextPage = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<IFormValues>({ criteriaMode: 'all' });
 
-  const submitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const submitHandler: SubmitHandler<IFormValues> = async (data) => {
     const res = await postFetcher(
       '/api/user',
-      `name=${encodeURIComponent(name)}&email=${encodeURIComponent(
-        email,
-      )}&password=${encodeURIComponent(password)}`,
+      `name=${encodeURIComponent(data.name)}&email=${encodeURIComponent(
+        data.email,
+      )}&password=${encodeURIComponent(data.password)}`,
     ).catch(() => ({ status: '500' }));
 
     if (res.status === '200') {
-      alert(`ユーザ：${name}を登録しました`);
+      alert(`ユーザ：${data.name}を登録しました`);
     } else {
-      alert(`ユーザ：${name}の登録に失敗しました`);
+      alert(`ユーザ：${data.name}の登録に失敗しました`);
     }
   };
 
-  const nameHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setName(e.target.value);
-  };
-
-  const emailHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
-  };
-
-  const passwordHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
+  const passwordReq = {
+    required: true,
+    minLength: 8,
   };
 
   return (
     <div>
       <h1 className='text-4xl font-bold m-4'>新規ユーザ作成</h1>
-      <form onSubmit={submitHandler}>
-        <div className='flex ml-6 mb-4'>
-          <label htmlFor='name' className='flex-none w-32 text-right'>
-            名前：
-          </label>
-          <input
-            type='text'
-            onChange={nameHandler}
-            className='border-gray-300 shadow-md px-2 py-1 mb-4 flex-none w-64'
-          />
-        </div>
-        <div className='flex ml-6 mb-4'>
-          <label htmlFor='email' className='flex-none w-32 text-right'>
-            メールアドレス：
-          </label>
-          <input
-            type='text'
-            onChange={emailHandler}
-            placeholder='xxxxxx@xxxx.co.jp'
-            className='border-gray-300 shadow-md px-2 py-1 mb-4 flex-none w-64'
-          />
-        </div>
-        <div className='flex ml-6 mb-4'>
-          <label htmlFor='password' className='flex-none w-32 text-right'>
-            パスワード：
-          </label>
-          <input
-            type='password'
-            onChange={passwordHandler}
-            className='border-gray-300 shadow-md px-2 py-1 mb-4 flex-none w-64'
-          />
-        </div>
+      <form onSubmit={handleSubmit(submitHandler)}>
+        <p className='ml-40 font-bold text-red-600'>
+          {errors.name?.type === 'required' && '入力が必須です'}
+        </p>
+        <InputForm
+          label='name'
+          placeholder=''
+          register={register}
+          required={{ required: true }}
+        ></InputForm>
+        <p className='ml-40 font-bold text-red-600'>
+          {errors.email?.type === 'required' && '入力が必須です'}
+        </p>
+        <InputForm
+          label='email'
+          placeholder='xxxxxx@xxxx.co.jp'
+          register={register}
+          required={{ required: true }}
+        ></InputForm>
+        <p className='ml-40 font-bold text-red-600'>
+          {errors.password?.type === 'required' && '入力が必須です'}
+        </p>
+        <p className='ml-40 font-bold text-red-600'>
+          {errors.password?.type === 'minLength' && '8文字以上で入力してください'}
+        </p>
+        <InputForm
+          label='password'
+          placeholder=''
+          register={register}
+          required={passwordReq}
+        ></InputForm>
         <div>
           <input
             type='submit'
@@ -81,5 +74,30 @@ const NewUserPage: NextPage = () => {
     </div>
   );
 };
+
+interface IFormValues {
+  name: string;
+  email: string;
+  password: string;
+}
+
+type InputProps = {
+  label: Path<IFormValues>;
+  placeholder: string;
+  register: UseFormRegister<IFormValues>;
+  required: any;
+};
+
+const InputForm = ({ label, placeholder, register, required }: InputProps) => (
+  <div className='flex ml-6 mb-4'>
+    <label className='flex-none w-32 text-right'>{label.toUpperCase()}:</label>
+    <input
+      {...register(label, { ...required })}
+      type={label}
+      placeholder={placeholder}
+      className='border-gray-300 shadow-md px-2 py-1 mb-4 flex-none w-64'
+    />
+  </div>
+);
 
 export default NewUserPage;
