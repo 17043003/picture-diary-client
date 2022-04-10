9e47faee96d811841f46da5302ca57e85da6c383
17043003 <ishizuka533725@gmail.com>
2022-04-10 17:42:39 +0900

    fix redirect when not autheticated, add not redirected path.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,30 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { parseCookies, setCookie, destroyCookie } from 'nookies';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Header, { navElement } from '../components/header';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [loggedIn, setLoggedIn] = useState(false);
   const router = useRouter();
-  useEffect(() => {
-    const cookies = parseCookies();
-    setLoggedIn(cookies['token'] != null);
-  });
+
+  const exceptPath = (path: string): boolean => {
+    const paths = ['/', '/login', '/user/new'];
+    return paths.includes(path);
+  }
 
   useEffect(() => {
-    if (!loggedIn) {
+    const cookies = parseCookies();
+
+    if(!exceptPath(router.pathname) && cookies['token'] == null) {
       router.push('/login');
     }
-  }, [loggedIn]);
+  })
 
   const elements: navElement[] = [
     { name: 'TOP', path: '/' },
     { name: 'POST', path: '/post' },
     { name: 'MYPAGE', path: '/user/mypage' },
-    loggedIn ? { name: 'LOGOUT', path: '' } : { name: 'LOGIN', path: '/login' },
   ];
 
   return (
