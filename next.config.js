75faf3306f18cdbd5f17fec93bf71dae6a059f2e
17043003 <ishizuka533725@gmail.com>
2022-04-19 00:44:29 +0900

    add env config to use amplify.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    BASEURL: process.env.BASEURL,
+    ACCESS_KEY: process.env.ACCESS_KEY,
+    SECRET_KEY: process.env.SECRET_KEY,
+    REGION: process.env.REGION,
+    BUCKET_NAME: process.env.BUCKET_NAME,
+  },
 };
 
 module.exports = nextConfig;
